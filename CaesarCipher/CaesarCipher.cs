9ac9f381e67cbb2f2dddc35151ec4e992df934a4
H Sharath Achar <sharathachar110@gmail.com>
2018-07-19 17:04:50 +0530

    seperated the test condition for capital and small letters

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,7 +7,39 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            //throw new NotImplementedException("You need to implement this function.");
+            string result = "";
+            if (shiftKey < 26 && shiftKey > 0)
+            {
+                for (int k=0; k<text.Length; k++)
+	            {
+                    int i = (int)text[k];
+
+                    if(i > 64 && i < 91)
+                    {
+                        i += shiftKey;
+                        if (i > 90 && i < 117)
+                        {
+                             i = i - 90 + 64;  
+                        }
+                    }
+                    else if (i > 96 && i < 123)
+                    {
+                        i += shiftKey;
+                        if (i > 122 && i < 149)
+                        {
+                            i = i - 122 + 96;
+                        }
+                    } 
+                    result += (char)i;
+	            }
+            }
+
+            else if (shiftKey == 0 || shiftKey == 26)
+                result = text;
+
+            return result;
+
         }
     }
 }
